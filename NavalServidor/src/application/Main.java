5464b03bb52ec0adabff0967d1eee516129877df
Danilo <Danilo@192.168.1.8>
2018-11-04 18:37:25 -0200

    nova inteface, menu e imagens nos botões

--- NavalServidor/src/application/Main.java
+++ NavalServidor/src/application/Main.java
@@ -4,18 +4,23 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 
 public class Main extends Application {
+	
 	@Override
 	public void start(Stage primaryStage) {
+		
 		try {
-			Parent root = FXMLLoader.load(getClass().getResource("/br/batalha/view/Tela.fxml"));
+			Parent root = FXMLLoader.load(getClass().getResource("/br/batalha/view/Menu.fxml"));
 			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.setTitle("Servidor Batalha Naval");
+//			primaryStage.setResizable(false);
+			primaryStage.getIcons().add(new Image("/images/anchora.png"));
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
@@ -25,4 +30,6 @@ public class Main extends Application {
 	public static void main(String[] args) {
 		launch(args);
 	}
+
+	
 }
